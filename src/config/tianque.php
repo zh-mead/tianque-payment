2f374e6135ac70992afec1b4c34aa2a2f2ea48b5
mead <751066209@qq.com>
2023-06-16 15:02:27 +0800

    init

--- src/config/tianque.php
+++ src/config/tianque.php
@@ -1,14 +1,14 @@
 <?php
 return [
     //机构编号
-    'orgId' => '08770583',
+    'orgId' => '',
     //商户编号
-    'mno' => '399230525900232',
+    'mno' => '',
     //微信子公众号
-    'subAppid' => 'wx0e09e453a535e660',
+    'subAppid' => '',
     //支付结果通知地址不上送则交易成功后，无异步交易结果通知
-    'notifyUrl' => env('APP_URL') . '/api/payments/tianque-notify',
-    'refundNotifyUrl' => env('APP_URL') . '/api/payments/tianque-refund-notify',
-    'privateKeyPath' => base_path() . '/tianque-cert/pri.key',
-    'publicKeyPath' => base_path() . '/tianque-cert/pub.key',
+    'notifyUrl' => '',
+    'refundNotifyUrl' => '',
+    'privateKeyPath' => '/tianque-cert/pri.key',
+    'publicKeyPath' => '/tianque-cert/pub.key',
 ];
